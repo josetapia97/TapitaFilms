71eab3b8b776905f18a79859e580b5bd369b817c
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-24 19:53:40 -0300

    First Upload

---
+++ android/app/src/main/kotlin/com/example/tapitafilms/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tapitafilms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:tapitafilms/presentation/screens/movies/home_screen.dart';
+
