7414533f1c277d6097daa80a9ec218e0778f70d5
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-26 20:01:48 -0300

    CardSwiper: animate_do

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:tapitafilms/domain/entities/movie.dart';
@@ -12,13 +13,11 @@ class MoviesSlideshow extends StatelessWidget {
       height: 210,
       width: double.infinity,
       child: Swiper(
-        viewportFraction: 0.8,
-        scale: 0.9,
-        autoplay: true,
-        itemCount: movies.length,
-        itemBuilder: (context, index) =>
-          _Slide(movie: movies[index])
-      ),
+          viewportFraction: 0.8,
+          scale: 0.9,
+          autoplay: true,
+          itemCount: movies.length,
+          itemBuilder: (context, index) => _Slide(movie: movies[index])),
     );
   }
 }
@@ -29,6 +28,29 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final decoration = BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black45, blurRadius: 10, offset: Offset(0, 10)),
+        ]);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+          decoration: decoration,
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.backdropPath,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const DecoratedBox(
+                        decoration: BoxDecoration(color: Colors.black12));
+                  }
+                  return FadeIn(child: child);
+                },
+              ))),
+    );
   }
 }
