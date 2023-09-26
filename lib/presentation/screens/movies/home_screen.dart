3d8f43f3fbb2da5bfaa3d93861095fcfd9722188
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-26 19:45:46 -0300

    CardSwiper: Implementacion

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tapitafilms/presentation/providers/movies/movies_providers.dart';
 import 'package:tapitafilms/presentation/widgets/shared/custom_appbar.dart';
+import 'package:tapitafilms/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -33,17 +34,8 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     return Column(
       children: [
-        CustomAppbar(),
-        Expanded(
-          child: ListView.builder(
-          itemCount: nowPlayingMovies.length,
-          itemBuilder: (context, index) {
-            final movie = nowPlayingMovies[index];
-            return ListTile(
-              title: Text(movie.title),
-            );
-          }),
-        )
+        const CustomAppbar(),
+        MoviesSlideshow(movies: nowPlayingMovies)
       ],
     );
   }
