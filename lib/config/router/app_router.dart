282fc02601b43bd408d34d2a9c86f84e5396b83d
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-28 18:03:02 -0300

    Deep link: volver atras

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,13 +1,21 @@
 import 'package:go_router/go_router.dart';
 import 'package:tapitafilms/presentation/screens/screens.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    ),
-  ]
-);
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+    routes: [
+       GoRoute(
+         path: 'movie-id/:id',
+         name: MovieScreen.name,
+         builder: (context, state) {
+           final movieId = state.pathParameters['id']?? 'no-id';
+            return MovieScreen(movieId: movieId);
+         },
+        ),  
+    ]
+  ),
+  
+]);
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,2 +1,2 @@
 export 'package:tapitafilms/presentation/screens/movies/home_screen.dart';
-
+export 'movies/movie_screen.dart';
