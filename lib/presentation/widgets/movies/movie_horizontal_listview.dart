f4bd4d1869e8b123e5a79eb1ee4d22976c82b734
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-28 18:02:43 -0300

    captura id por pelicula

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tapitafilms/config/helpers/human_formats.dart';
 import 'package:tapitafilms/domain/entities/movie.dart';
 
@@ -106,7 +107,10 @@ class _Slide extends StatelessWidget {
                           ),
                         );
                       }
-                      return FadeIn(child: child);
+                      return GestureDetector(
+                        onTap: ()=> context.push('/movie-id/${movie.id}'),
+                        child: FadeIn(child: child),
+                      );
                     },
                   ),
                 ),
