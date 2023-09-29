d656e6dca3d02add072c8ed385a6e278d4af9918
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-09-29 18:49:41 -0300

    Interfaz: Titulo, descripcion y generos

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -28,14 +28,143 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
           child: CircularProgressIndicator(
             strokeWidth: 2,
           ),
-
         ),
       );
     }
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MovieID: ${widget.movieId}'),
-      ),
+        body: CustomScrollView(
+      physics: const ClampingScrollPhysics(),
+      slivers: [
+        _CustomSliverAppbar(
+          movie: movie,
+        ),
+        SliverList(delegate: SliverChildBuilderDelegate((
+          context, index) =>_MovieDetails(movie: movie),
+          childCount: 1))
+      ],
+    ));
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+      final Movie movie;
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyle = Theme.of(context).textTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(padding: const EdgeInsets.all(10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(movie.posterPath,
+              width: size.width *0.3)
+              
+            ),
+            const SizedBox(width: 10,),
+            //?Descripcion
+
+            SizedBox(
+              width: (size.width-70)*0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:[
+                  Text(movie.title, style: textStyle.titleLarge),
+                  const SizedBox(height: 10,),
+                  Text(movie.overview),
+                ]
+              ),
+            )
+          ],
+        ),),
+
+        //generos de la pelicula
+        Padding(padding: const EdgeInsets.all(8),
+        child: Wrap(
+          children: [
+            ...movie.genreIds.map((gender) => Container(
+              margin: const EdgeInsets.only(right: 10),
+              child: Chip(
+                label: Text(gender),
+                shape: RoundedRectangleBorder(
+                  borderRadius:BorderRadius.circular(20) ),),
+            ))
+          ],
+         ),),
+
+
+        const SizedBox(height: 100,)
+      ],
+    );
+  }
+}
+
+class _CustomSliverAppbar extends StatelessWidget {
+  const _CustomSliverAppbar({required this.movie});
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 30,vertical: 25),
+        title: Text(movie.title, style: const TextStyle(fontSize: 20),
+        textAlign: TextAlign.start),
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(
+              movie.posterPath,
+              fit: BoxFit.cover,),
+              
+            ),
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.7,1.0],
+                    colors: [
+                      Colors.transparent,
+                      Colors.black87
+                    ]
+                    )
+                ) ,
+                ),
+                
+              
+            ),
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.0,0.3],
+                    colors: [
+                      Colors.black87,
+                      Colors.transparent,
+                      
+                    ]
+                    )
+                ) ,)
+            ),
+          ],
+        ),
+        ),
+      
     );
   }
 }
