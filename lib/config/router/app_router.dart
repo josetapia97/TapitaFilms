8a809642942ad1a240d9905cbd9c3f304cc33408
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-10-03 16:29:34 -0300

    router: ShellRoute (navbar)

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,20 +3,50 @@ import 'package:tapitafilms/presentation/screens/screens.dart';
 import 'package:tapitafilms/presentation/views/views.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-    path: '/',
-    name: HomeScreen.name,
-    builder: (context, state) => const HomeScreen(childView: HomeView()),
+  ShellRoute(
+    builder: (context, state, child) {
+      return HomeScreen(childView: child);
+    },
     routes: [
-       GoRoute(
-         path: 'movie/:id',
-         name: MovieScreen.name,
-         builder: (context, state) {
-           final movieId = state.pathParameters['id']?? 'no-id';
-            return MovieScreen(movieId: movieId);
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const HomeView();
+        },
+        routes: [
+            GoRoute(
+             path: 'movie/:id',
+             name: MovieScreen.name,
+             builder: (context, state) {
+               final movieId = state.pathParameters['id']?? 'no-id';
+                return MovieScreen(movieId: movieId);
          },
-        ),  
+        ),
+        ]
+      ),
+      GoRoute(
+        path: '/favorites',
+        builder: (context, state) {
+          return const FavoritesView();
+        },
+      )
     ]
-  ),
-  
+  )
+
+  //? Rutas padre/hijo
+  // GoRoute(
+  //   path: '/',
+  //   name: HomeScreen.name,
+  //   builder: (context, state) => const HomeScreen(childView: HomeView()),
+  //   routes: [
+      //  GoRoute(
+      //    path: 'movie/:id',
+      //    name: MovieScreen.name,
+      //    builder: (context, state) {
+      //      final movieId = state.pathParameters['id']?? 'no-id';
+      //       return MovieScreen(movieId: movieId);
+      //    },
+      //   ),
+  //   ]
+  // ),
 ]);
