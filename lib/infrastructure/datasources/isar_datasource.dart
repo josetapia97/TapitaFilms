6087a9ddb36a9e668f8077731a34914e24efe7ae
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-10-05 20:48:45 -0300

    Isar: datasource & repository impl

---
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:tapitafilms/domain/datasources/local_storage_datasource.dart';
+import 'package:tapitafilms/domain/entities/movie.dart';
+
+class IsarDatasource extends LocalStorageDatasource{
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+
+}
