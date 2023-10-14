34333dc265798bb8bf9e13ea5bf1e7df64f56296
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-10-14 20:21:42 -0300

    Tema configurado

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  ThemeData getTheme() => ThemeData(
+  ThemeData getLightTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: const Color(0xff2862f5)
+    colorSchemeSeed: const Color(0xff2862f5),
+    brightness: Brightness.light,
+  );
+
+  ThemeData getDarkTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Colors.white,
+    brightness: Brightness.dark,
   );
 }
+
+
+
+
+
