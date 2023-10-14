21593afbd06591e5f6dfc499e5b8beb8c455a31c
Jose Tapia <jose.tapia@alu.ucm.cl>
2023-10-14 20:53:36 -0300

    Loader

--- lib/presentation/widgets/movies/movie_masonry.dart
+++ lib/presentation/widgets/movies/movie_masonry.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:tapitafilms/domain/entities/movie.dart';
@@ -6,25 +7,30 @@ import 'package:tapitafilms/presentation/widgets/movies/movie_poster_link.dart';
 class MovieMasonry extends StatefulWidget {
   final List<Movie> movies;
   final VoidCallback? loadNextPage;
-  const MovieMasonry({super.key, required this.movies, this.loadNextPage});
+  const MovieMasonry({Key? key, required this.movies, this.loadNextPage})
+      : super(key: key);
 
   @override
   State<MovieMasonry> createState() => _MovieMasonryState();
 }
 
 class _MovieMasonryState extends State<MovieMasonry> {
+
   final scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    scrollController.addListener(() {
-      if (widget.loadNextPage == null) return;
+    
+    scrollController.addListener(() { 
+      if ( widget.loadNextPage == null ) return;
 
-      if((scrollController.position.pixels + 100)>=scrollController.position.maxScrollExtent){
+      if ( (scrollController.position.pixels + 100) >= scrollController.position.maxScrollExtent ) {
         widget.loadNextPage!();
       }
+
     });
+
   }
 
   @override
@@ -39,22 +45,22 @@ class _MovieMasonryState extends State<MovieMasonry> {
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: MasonryGridView.count(
         controller: scrollController,
-        itemCount: widget.movies.length,
-        crossAxisCount: 3,
+        crossAxisCount: 3, 
         mainAxisSpacing: 10,
         crossAxisSpacing: 10,
+        itemCount: widget.movies.length,
         itemBuilder: (context, index) {
-          if (index == 1) {
+
+          if ( index == 1 ) {
             return Column(
               children: [
-                const SizedBox(
-                  height: 40,
-                ),
-                MoviePosterLink(movie: widget.movies[index])
+                const SizedBox(height: 20 ),
+                MoviePosterLink( movie: widget.movies[index] )
               ],
             );
           }
-          return MoviePosterLink(movie: widget.movies[index]);
+          
+          return MoviePosterLink( movie: widget.movies[index] );
         },
       ),
     );
